67b00db578c61a342bd6f272b217ac2c4e980438
Ciro Zullo <211477495+cirozullo@users.noreply.github.com>
2025-12-30 15:23:38 +0100

    Migliorie minori

--- src/main/java/com/storyfinder/AvvioGioco.java
+++ src/main/java/com/storyfinder/AvvioGioco.java
@@ -1,36 +1,16 @@
 import com.google.gson.Gson;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import org.apache.commons.io.IOUtils;
 
 public class AvvioGioco {
 
     public static void main(String[] args) {
         Gson gson = new Gson();
+        String NOME_STORIA = "storia1.json"; // Si cambia a piacere
 
-        ClassLoader classloader =
-            Thread.currentThread().getContextClassLoader();
+        Storia storia = gson.fromJson(
+            GestoreStoria.getStory(NOME_STORIA),
+            Storia.class
+        );
 
-        String resourcePath = "storia1.json";
-        InputStream is = classloader.getResourceAsStream(resourcePath);
-
-        if (is == null) {
-            System.err.println("File inesistente");
-            return;
-        }
-
-        try (InputStream inputStream = is) {
-            String result = IOUtils.toString(
-                inputStream,
-                StandardCharsets.UTF_8
-            );
-
-            Storia storia = gson.fromJson(result, Storia.class);
-
-            System.out.println(storia);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(storia);
     }
 }
--- src/main/java/com/storyfinder/GestoreStoria.java
+++ src/main/java/com/storyfinder/GestoreStoria.java
@@ -1,1 +1,32 @@
-public class GestoreStoria {}
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.IOUtils;
+
+public class GestoreStoria {
+
+    /**
+     *  Carica una storia specifica e converti l'InputStream in stringa da passare
+     *  a gson. Richiede un file json con struttura specifica
+     *
+     *  @param storyName Il nome del file della storia da caricare
+     *  @return Il file JSON in formato stringa
+     */
+    public static String getStory(String storyName) {
+        ClassLoader classloader =
+            Thread.currentThread().getContextClassLoader();
+
+        InputStream is = classloader.getResourceAsStream(storyName);
+        if (is == null) {
+            System.err.println("File inesistente");
+        }
+
+        String result = null;
+        try (InputStream inputStream = is) {
+            result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
